2d0fcf52c42e489fc4c3a31542c5940b6cb36821
Kazi-Tanvir <kazimainulislamtanvir@gmail.com>
2026-09-11 20:20:33 +0600

    Practice : Chapter 7 : Problem 10

---
+++ PRACTICE/C BOOK/CHAPTER_06/10_Example_641.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char *ptr = "stop\n";
+    char str[80] = {0};
+
+    while(strcmp(ptr,str)){
+        printf("Enter your string : ");
+        fgets(str,sizeof(str),stdin);
+        }
+    return 0;
+}
