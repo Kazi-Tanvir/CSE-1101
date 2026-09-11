850ab27ec5cd1743bf98e94fd79a880961e91e3d
Kazi-Tanvir <kazimainulislamtanvir@gmail.com>
2026-09-11 17:06:57 +0600

    Practice : Chapter 6 : Problem 06

--- PRACTICE/C BOOK/CHAPTER_06/05_Example_622.c
+++ PRACTICE/C BOOK/CHAPTER_06/05_Example_622.c
@@ -3,12 +3,12 @@
 int main(){
     int *p , q = 0;
     p = &q;
+    
     *p++;
     printf("%d %d %u \n", q,*p,p);
     *p--;
     (*p)++;
     printf("%d %d %u \n", q,*p,p);
-
 
     return 0;
 }
---
+++ PRACTICE/C BOOK/CHAPTER_06/06_Exercise_621.c
@@ -0,0 +1,6 @@
+// int *p, i;
+
+// p = &i;
+
+// p = p * 8;  [ p is a pointer and we cannot multiply in pointer arithmetic]
+// *p = *p*8 is valid
