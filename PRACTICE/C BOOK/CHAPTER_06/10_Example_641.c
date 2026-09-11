99b15bccb3e742d818228c21f9c7e9f48cd54623
Kazi-Tanvir <kazimainulislamtanvir@gmail.com>
2026-09-11 20:23:14 +0600

    Practice : Chapter 7 : Problem 11

--- PRACTICE/C BOOK/CHAPTER_06/10_Example_641.c
+++ PRACTICE/C BOOK/CHAPTER_06/10_Example_641.c
@@ -2,12 +2,12 @@
 #include<string.h>
 
 int main(){
-    char *ptr = "stop\n";
+    char *ptr = "stop";
     char str[80] = {0};
 
     while(strcmp(ptr,str)){
         printf("Enter your string : ");
         fgets(str,sizeof(str),stdin);
-        }
+    }
     return 0;
 }
---
+++ PRACTICE/C BOOK/CHAPTER_06/11_Example_642.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char *InsDisk = "Insert disk into drive A, then press ENTER\n";
+
+    printf(InsDisk);
+    printf(InsDisk);
+    printf(InsDisk);
+}
