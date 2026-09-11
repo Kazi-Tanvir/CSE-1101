5ff5291a0a7741a421b9bb6df81fc16397e334d1
Kazi-Tanvir <kazimainulislamtanvir@gmail.com>
2026-09-11 17:26:39 +0600

    Practice : Chapter 7 : Problem 08

---
+++ PRACTICE/C BOOK/CHAPTER_06/08_Example_632.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str[] = "This Aint a Reverse String";
+    char rStr[80],*p1,*p2;
+
+    p1 = str + strlen(str) - 1;
+    p2 = rStr;
+    
+    while (p1>= str) *p2++ = *p1--;
+    *p2 = '\0';
+
+    printf(str);
+    printf("\n");
+    printf(rStr);
+
+    return 0;
+    
+}
