fe8358bd599f73df73f66523eef06b971bbaf31f
Kazi-Tanvir <kazimainulislamtanvir@gmail.com>
2026-09-11 17:09:07 +0600

    Practice : Chapter 6 : Problem 06

--- PRACTICE/C BOOK/CHAPTER_06/06_Exercise_621.c
+++ PRACTICE/C BOOK/CHAPTER_06/06_Exercise_621.c
@@ -1,6 +1,12 @@
+// Exercise 1 : Answer
+
 // int *p, i;
-
 // p = &i;
-
 // p = p * 8;  [ p is a pointer and we cannot multiply in pointer arithmetic]
 // *p = *p*8 is valid
+
+// Exercise 2 : Answer 
+// Yes 
+
+// Exercise 3 : 
+// 100 + 4*2 = 108
